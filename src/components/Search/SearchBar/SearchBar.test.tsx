d59c5200dce8fc8eb497bf0e1c9f58f74917a281
Viktoryia Karotkaya <viki.karotkaya@gmail.com>
2022-08-17 13:18:42 -0400

    Fire events in integration test for SearchBar

--- src/components/Search/SearchBar/SearchBar.test.tsx
+++ src/components/Search/SearchBar/SearchBar.test.tsx
@@ -1,5 +1,6 @@
 import renderer from "react-test-renderer";
 import { act } from "react-dom/test-utils";
+import { createMemoryHistory } from "history";
 import { MemoryRouter } from "react-router-dom";
 import ErrorBoundary from "HOC/ErrorBoundary/ErrorBoundary";
 import { Provider } from "react-redux";
@@ -14,67 +15,46 @@ import { configureStore } from "@reduxjs/toolkit";
 import moviesReducer from "../../../features/movies/moviesSelector";
 import modalWindowReducer from "../../../features/modalWindow/modalWindowSelector";
 import { waitFor } from "@testing-library/react";
+import { getMovies } from "api/movieService";
+import userEvent from "@testing-library/user-event";
 
 describe("SearchBar testing", () => {
   test("snapshot for SearchBar", () => {
-    const store = configureStore({
-      reducer: { movies: moviesReducer, modalWindow: modalWindowReducer },
-    });
-    const tree = renderer
-      .create(
-        <ErrorBoundary>
-          <Provider store={store}>
-            <MemoryRouter>
-              <SearchBar />
-            </MemoryRouter>
-          </Provider>
-        </ErrorBoundary>
-      )
-      .toJSON();
-    expect(tree).toMatchSnapshot();
-    // const { baseElement } = render(
-    //   <ErrorBoundary>
-    //     <Provider store={store}>
-    //       <MemoryRouter>
-    //         <SearchBar />
-    //       </MemoryRouter>
-    //     </Provider>
-    //   </ErrorBoundary>
-    // );
-    // expect(baseElement).toMatchSnapshot();
-  });
-  test("fire event", async () => {
-    const handleClick = jest.fn();
-    renderWithProviders(
+    const { baseElement } = renderWithProviders(
       <ErrorBoundary>
         <MemoryRouter>
           <SearchBar />
         </MemoryRouter>
       </ErrorBoundary>
     );
-    await waitFor(() => {
-      fireEvent.change(
-        screen.getByPlaceholderText("What do you want to watch?"),
-        {
-          target: {
-            value: "mockname",
-          },
-        }
+    expect(baseElement).toMatchSnapshot();
+  });
+  test("fire input event, search event and calling fetchMovies", async () => {
+    const mock = jest.fn();
+
+    let result = mock("getMovies");
+    act(() => {
+      renderWithProviders(
+        <ErrorBoundary>
+          <MemoryRouter>
+            <SearchBar />
+          </MemoryRouter>
+        </ErrorBoundary>
       );
     });
 
     await waitFor(() => {
-      fireEvent.click(screen.getByText(/Search/i));
+      userEvent.type(
+        screen.getByPlaceholderText("What do you want to watch?"),
+        "mockname"
+      );
     });
-    // const { baseElement } = render(
-    //   <ErrorBoundary>
-    //     <Provider store={store}>
-    //       <MemoryRouter>
-    //         <SearchBar />
-    //       </MemoryRouter>
-    //     </Provider>
-    //   </ErrorBoundary>
-    // );
-    // expect(baseElement).toMatchSnapshot();
+
+    await act(async () => {
+      await waitFor(() => {
+        fireEvent.click(screen.getByText(/Search/i));
+      });
+    });
+    expect(mock).toHaveBeenCalledTimes(1);
   });
 });
